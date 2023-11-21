62a3b86f1c1aea217604bf4bdcc105c2926f389c
2535Nova <petit11158@lasalle63.fr>
2023-11-21 16:59:32 +0100

    ajout exemple

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,39 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Commentaire;
+use App\Entity\Post;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
+        $User = new User();
+        $User->setUsername("root");
+        $User->setRoles(["ROLE_ADMIN"]);
+        $User->setPassword("root");
+        $User->setBan(FALSE);
+        $manager->persist($User);
+
+        $Post = new Post();
+        $Post->setUser($User);
+        $Post->setIslock(FALSE);
+        $Post->setDescription(" ceci est un poste de test");
+        $manager->persist($Post);
+
+        $commantaire = new Commentaire();
+        $commantaire->setUserId($User);
+        $commantaire->setPostId($Post);
+        $commantaire->setCommentaireId($commantaire);
+        $commantaire->setContent("ceci est un test");
+        $date = new \DateTime ("2000-02-02");
+        $commantaire->setDate($date);
+        $manager->persist($commantaire);
         $manager->flush();
     }
 }
