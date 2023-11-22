7e34b0b3f01367eb7063aebd053148628ed112c2
2535Nova <petit11158@lasalle63.fr>
2023-11-22 11:24:16 +0100

    ajout des clé

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -20,12 +20,14 @@ use OpenApi\Attributes as OA;
 use App\Service\JsonConverter;
 use App\Entity\User;
 
-class UserController extends AbstractController {
+class UserController extends AbstractController
+{
 
     private $jsonConverter;
     private $passwordHasher;
 
-    public  function __construct(JsonConverter $jsonConverter, UserPasswordHasherInterface $passwordHasher) {
+    public  function __construct(JsonConverter $jsonConverter, UserPasswordHasherInterface $passwordHasher)
+    {
         $this->passwordHasher = $passwordHasher;
         $this->jsonConverter = $jsonConverter;
     }
@@ -48,7 +50,8 @@ class UserController extends AbstractController {
         )
     )]
     #[OA\Tag(name: 'utilisateurs')]
-    public function logUser(ManagerRegistry $doctrine, JWTTokenManagerInterface $JWTManager) {
+    public function logUser(ManagerRegistry $doctrine, JWTTokenManagerInterface $JWTManager)
+    {
         $request = Request::createFromGlobals();
         $data = json_decode($request->getContent(), true);
 
@@ -78,7 +81,8 @@ class UserController extends AbstractController {
         content: new OA\JsonContent(ref: new Model(type: User::class))
     )]
     #[OA\Tag(name: 'utilisateurs')]
-    public function getUtilisateur(JWTEncoderInterface $jwtEncoder, Request $request) {
+    public function getUtilisateur(JWTEncoderInterface $jwtEncoder, Request $request)
+    {
         $tokenString = str_replace('Bearer ', '', $request->headers->get('Authorization'));
 
         $user = $jwtEncoder->decode($tokenString);
@@ -97,11 +101,72 @@ class UserController extends AbstractController {
         )
     )]
     #[OA\Tag(name: 'utilisateurs')]
-    public function getAllUsers(ManagerRegistry $doctrine) {
+    public function getAllUsers(ManagerRegistry $doctrine)
+    {
 
         $entityManager = $doctrine->getManager();
 
         $users = $entityManager->getRepository(User::class)->findAll();
         return new Response($this->jsonConverter->encodeToJson($users));
+    }
+
+
+    private function createUser()
+    {
+        $input = (array) json_decode(file_get_contents('php://input'), true);
+        if (empty($input["username"]) || empty($input["password"]) || empty($input["avatar"])) {
+            return $this->unprocessableEntityResponse();
+        }
+        // La chaîne image base64
+        $base64_image = $input["avatar"];
+
+        // Fonction pour déterminer le format de l'image en fonction des premiers octets
+        // Utilise preg_match pour extraire le format de l'image depuis la chaîne base64
+        if (preg_match('#^.*?base64,#', $base64_image, $matches)) {
+            $imageFormat = $matches[0]; // Obtient le format de l'image
+
+            // Divise la chaîne en utilisant la virgule comme séparateur
+            $parts = explode(';', $base64_image);
+
+            if (count($parts) > 0) {
+                // Extrayez la partie finale après la dernière barre oblique
+                $imageFormat = end(explode('/', $parts[0]));
+
+                // Génère un nom de fichier unique
+                $imageName = $input["nom"] . "." . $imageFormat;
+
+                // Spécifie le chemin de destination pour enregistrer l'image
+                $destinationPath = "src/images/" . $imageName;
+
+                // Extrait les données de l'image (après la virgule)
+                $imageData = substr($base64_image, strpos($base64_image, ',') + 1);
+
+                // Décode la chaîne base64 en binaire
+                $binaryData = base64_decode($imageData);
+            }
+
+            if ($binaryData !== false) {
+                // Enregistre l'image sur le serveur
+                file_put_contents($destinationPath, $binaryData);
+            }
+
+
+            $user = new User($input["username"], $input["password"], $input["avatar"]);
+            
+
+
+            $response['status_code_header'] = $_SERVER['SERVER_PROTOCOL'] . ' 201 Created';
+            $response['body'] = json_encode($user);
+            return $response;
+        }
+    }
+
+    private function unprocessableEntityResponse()
+    {
+        $response['status_code_header'] = $_SERVER['SERVER_PROTOCOL'] . ' 422 Unprocessable Entity';
+        $response['body'] = json_encode([
+            'error' => 'Invalid input'
+        ]);
+        return $response;
     }
 }
