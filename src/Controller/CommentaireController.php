47bf8b7e1b2f8c29a51604fff771ea716191abfe
2535Nova <petit11158@lasalle63.fr>
2023-11-29 09:40:04 +0100

    Correctif doc Commentaire

--- src/Controller/CommentaireController.php
+++ src/Controller/CommentaireController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use App\Entity\Commentaire;
@@ -13,11 +14,13 @@ use Nelmio\ApiDocBundle\Annotation\Model;
 use App\Entity\User;
 use App\Entity\Post;
 
+
 use OpenApi\Attributes as OA;
 
 
-class CommentaireController extends AbstractController{
-    
+class CommentaireController extends AbstractController
+{
+
     private $jsonConverter;
 
     public function __construct(JsonConverter $jsonConverter)
@@ -73,23 +76,10 @@ class CommentaireController extends AbstractController{
         content: new OA\JsonContent(
             type: 'object',
             properties: [
-                new OA\Property(property: 'content', type: 'string'),
-                new OA\Property(
-                    property: 'user',
-                    type: 'array',
-                    items: new OA\Items(ref: new Model(type: User::class))
-                ),
-                new OA\Property(
-                    property: 'post',
-                    type: 'array',
-                    items: new OA\Items(ref: new Model(type: Post::class))
-                ),
-                new OA\Property(
-                    property: 'commentaire',
-                    type: 'array',
-                    items: new OA\Items(ref: new Model(type: Commentaire::class))
-                ),
-                new OA\Property(property: "date", type: "string")
+                new OA\Property(property: "user_id_id", type: "number"),
+                new OA\Property(property: "post_id_id", type: "number"),
+                new OA\Property(property: "commentaire_id_id", type: "number"),
+                new OA\Property(property: "content", type: "string"),
             ]
         )
     )]
@@ -104,17 +94,35 @@ class CommentaireController extends AbstractController{
     )]
     public function addCommentaire(Request $request, ManagerRegistry $doctrine): Response
     {
-        $data = $this->jsonConverter->decodeFromJSon($request->getContent(), Commentaire::class);
+        $data = json_decode($request->getContent(), true);
 
         $entityManager = $doctrine->getManager();
 
+        // Récupérez l'utilisateur et le post
+        $user = $entityManager->getRepository(User::class)->find($data["user_id_id"]);
+        $post = $entityManager->getRepository(Post::class)->find($data["post_id_id"]);
+
+        // Assurez-vous que l'identifiant du commentaire parent est défini
+        $com = null;
+        if (isset($data["commentaire_id_id"])) {
+            $com = $entityManager->getRepository(Commentaire::class)->find($data["commentaire_id_id"]);
+
+            // Assurez-vous que l'entité Commentaire parent existe
+            if (!$com) {
+                return new JsonResponse(['message' => 'Commentaire parent non trouvé'], Response::HTTP_NOT_FOUND);
+            }
+        }
+
         // Créer une nouvelle instance de l'entité Commentaire
         $commentaire = new Commentaire();
-        $commentaire->setUserId($data["user"]);
+        $commentaire->setUserId($user);
         $commentaire->setContent($data["content"]);
-        $commentaire->setDate($data["date"]);
-        $commentaire->setPostId($data["post"]);
-        $commentaire->setCommentaireId($data["commentaire"]);
+
+        // Définir la date et l'heure actuelles
+        $commentaire->setDate(new \DateTime());
+
+        $commentaire->setPostId($post);
+        $commentaire->setCommentaireId($com);
 
         // Enregistrez le nouveau commentaire
         $entityManager->persist($commentaire);
@@ -124,70 +132,49 @@ class CommentaireController extends AbstractController{
     }
 
 
-    #[Route('/api/commentaire', methods: ['PUT'])]
+
+    #[Route('/api/commentaire/{id}', methods: ['PUT'])]
     #[OA\RequestBody(
         required: true,
         content: new OA\JsonContent(
             type: 'object',
             properties: [
-                new OA\Property(property: 'content', type: 'string'),
-                new OA\Property(
-                    property: 'user',
-                    type: 'array',
-                    items: new OA\Items(ref: new Model(type: User::class))
-                ),
-                new OA\Property(
-                    property: 'post',
-                    type: 'array',
-                    items: new OA\Items(ref: new Model(type: Post::class))
-                ),
-                new OA\Property(
-                    property: 'commentaire',
-                    type: 'array',
-                    items: new OA\Items(ref: new Model(type: Commentaire::class))
-                ),
-                new OA\Property(property: "date", type: "string")
+                new OA\Property(property: "content", type: "string"),
             ]
         )
     )]
     #[OA\Tag(name: 'Commentaires')]
-    #[OA\Parameter(
-        name: 'id',
-        in: 'query',
-        description: 'The field used to order rewards',
-        schema: new OA\Schema(type: 'int')
-    )]
     #[OA\Response(
         response: 200,
-        description: 'modifie un commentaire par ID',
+        description: 'Modifie un commentaire par ID',
         content: new OA\JsonContent(
             type: 'array',
             items: new OA\Items(ref: new Model(type: Commentaire::class))
         )
     )]
     public function updateCommentaire(int $id, Request $request, ManagerRegistry $doctrine): Response
-    {
-        $entityManager = $doctrine->getManager();
-        $commentaire = $entityManager->getRepository(Commentaire::class)->find($id);
+{
+    $entityManager = $doctrine->getManager();
+    $commentaire = $entityManager->getRepository(Commentaire::class)->find($id);
 
-        if (!$commentaire) {
-            return new JsonResponse(['error' => 'Commentaire non trouvé.'], Response::HTTP_NOT_FOUND);
-        }
-
-        $data = $this->jsonConverter->decodeFromJSon($request->getContent(), Commentaire::class);
-
-        // Mettez à jour les propriétés du commentaire en fonction des données de la requête
-        $commentaire->setUserId($data["user"]);
-        $commentaire->setContent($data["content"]);
-        $commentaire->setDate($data["date"]);
-        $commentaire->setPostId($data["post"]);
-        $commentaire->setCommentaireId($data["commentaire"]);
-
-        // Enregistrez les modifications
-        $entityManager->flush();
-
-        return new JsonResponse($this->jsonConverter->encodeToJson($commentaire));
+    if (!$commentaire) {
+        return new JsonResponse(['error' => 'Commentaire non trouvé.'], Response::HTTP_NOT_FOUND);
     }
+
+    $data = json_decode($request->getContent(), true);
+
+    // Mettez à jour les propriétés du commentaire en fonction des données de la requête
+    $commentaire->setContent($data["content"]);
+    // Définir la date et l'heure actuelles
+    $commentaire->setDate(new \DateTime());
+
+    // Enregistrez les modifications
+    $entityManager->flush();
+
+    return new JsonResponse($this->jsonConverter->encodeToJson($commentaire));
+}
+    
+
 
 
     #[Route('/api/commentaire/{id}', methods: ['DELETE'])]
@@ -212,4 +199,3 @@ class CommentaireController extends AbstractController{
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
-
