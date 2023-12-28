724e976ccced1206cf65f771177adfb9a18206bc
2535Nova <petit11158@lasalle63.fr>
2023-12-28 15:31:07 +0100

    update like

--- src/Controller/LikeController.php
+++ src/Controller/LikeController.php
@@ -104,45 +104,52 @@ class LikeController extends AbstractController
     {
         $entityManager = $doctrine->getManager();
         $like = $entityManager->getRepository(Like::class)->find($id);
-
+    
         if (!$like) {
             return new Response($this->jsonConverter->encodeToJson(['message' => 'Like non trouvé']), Response::HTTP_NOT_FOUND);
         }
-
+    
         $user = $security->getUser();
-
+    
         // Vérifier si l'utilisateur actuel est le propriétaire du like
         if (!$security->isGranted('ROLE_ADMIN') && $user !== $like->getUser()) {
             return new JsonResponse(['error' => 'Vous n\'êtes pas autorisé à mettre à jour ce like.'], Response::HTTP_FORBIDDEN);
         }
-
-        // Update the like based on request data
+    
+        // Vérifier si le post n'appartient pas à l'utilisateur
+        $post = $like->getPost();
+        if ($post->getUser() === $user) {
+            return new JsonResponse(['error' => 'Vous ne pouvez pas mettre à jour le like de votre propre post.'], Response::HTTP_FORBIDDEN);
+        }
+    
+        // Mise à jour du like en fonction des données de la requête
         $data = json_decode($request->getContent(), true);
-
-        // Assuming you have a 'content' field in the Like entity
+    
+        // Supposons que vous ayez un champ 'content' dans l'entité Like
         $like->setIslike($data["islike"]);
-
+    
         $entityManager->flush();
-
+    
         // Charger explicitement les entités User et Post pour la réponse
         $user = $like->getUser();
         $post = $like->getPost();
-
+    
         $likeData = [
             'id' => $like->getId(),
             'username' => $user ? $user->getUsername() : null, // Ajouter l'ID de l'utilisateur
             'post_id' => $post ? $post->getId() : null, // Ajouter l'ID du post
             'islike' => $like->isIslike(),
         ];
-
+    
         $data = $this->serializer->serialize(
             $likeData,
             'json',
             [AbstractNormalizer::GROUPS => ['like']]
         );
-
+    
         return new Response($data);
     }
+    
 
 
     #[Route('/api/like/{id}', methods: ['DELETE'])]
@@ -220,48 +227,54 @@ class LikeController extends AbstractController
     {
         $entityManager = $doctrine->getManager();
         $data = json_decode($request->getContent(), true);
-
+    
         // Vérifiez que les clés attendues sont présentes dans le tableau $data
         if (!isset($data["user_id"]) || !isset($data["islike"]) || !isset($data["post_id"])) {
-            return new Response($this->jsonConverter->encodeToJson(['message' => 'Donnees manquantes']), Response::HTTP_BAD_REQUEST);
+            return new Response($this->jsonConverter->encodeToJson(['message' => 'Données manquantes']), Response::HTTP_BAD_REQUEST);
         }
-
+    
         // Récupérez l'utilisateur et le post
         $user = $entityManager->getRepository(User::class)->find($data["user_id"]);
         $post = $entityManager->getRepository(Post::class)->find($data["post_id"]);
-
+    
         // Vérifiez que l'utilisateur et le post existent
         if (!$user || !$post) {
-            return new Response($this->jsonConverter->encodeToJson(['message' => 'Utilisateur ou post non trouve']), Response::HTTP_NOT_FOUND);
+            return new Response($this->jsonConverter->encodeToJson(['message' => 'Utilisateur ou post non trouvé']), Response::HTTP_NOT_FOUND);
         }
-
+    
+        // Vérifiez que le post n'appartient pas à l'utilisateur
+        if ($post->getUser() === $user) {
+            return new Response($this->jsonConverter->encodeToJson(['message' => 'Vous ne pouvez pas aimer votre propre post.']), Response::HTTP_BAD_REQUEST);
+        }
+    
         // Créez l'objet Like en utilisant les entités récupérées
         $like = new Like();
         $like->setUser($user);
         $like->setIslike($data["islike"]);
         $like->setPost($post);
-
+    
         // Persistez et flush l'entité
         $entityManager->persist($like);
         $entityManager->flush();
-
+    
         // Charger explicitement les entités User et Post pour la réponse
         $user = $like->getUser();
         $post = $like->getPost();
-
+    
         $likeData = [
             'id' => $like->getId(),
             'username' => $user ? $user->getUsername() : null, // Ajouter l'ID de l'utilisateur
             'post_id' => $post ? $post->getId() : null, // Ajouter l'ID du post
             'islike' => $like->isIslike(),
         ];
-
+    
         $data = $this->serializer->serialize(
             $likeData,
             'json',
             [AbstractNormalizer::GROUPS => ['like']]
         );
-
+    
         return new Response($data, Response::HTTP_CREATED);
     }
+    
 }
